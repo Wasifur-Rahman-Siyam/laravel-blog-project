1dd6d0f8ab42696be92dd645646085dc81e5299d
MD. WASIFUR RAHMAN SIYAM <wasifurrahmansiyam@gmail.com>
2023-08-12 23:54:32 -0700

    fixin in comment modal

--- resources/views/frontend/Posts/index.blade.php
+++ resources/views/frontend/Posts/index.blade.php
@@ -78,7 +78,7 @@
                   <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                       <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Write Your Comment: {{$post->id}}</h1>
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Write Your Comment:</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                       </div>
                       <div class="modal-body">
--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -77,7 +77,7 @@
                   <div class="modal-dialog modal-dialog-centered">
                       <div class="modal-content">
                         <div class="modal-header">
-                          <h1 class="modal-title fs-5" id="exampleModalLabel">Write Your Comment: {{$randomPost->id}}</h1>
+                          <h1 class="modal-title fs-5" id="exampleModalLabel">Write Your Comment:</h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
